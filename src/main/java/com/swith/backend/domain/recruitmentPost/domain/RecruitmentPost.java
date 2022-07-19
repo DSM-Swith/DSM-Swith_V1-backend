7a2e51924b3b49febdcc20fc64b731ff2304e397
luck1306 <luck1306@naver.com>
2022-07-19 20:07:59 +0900

    create (recruitment-post) Entity 구현

---
+++ src/main/java/com/swith/backend/domain/recruitmentPost/domain/RecruitmentPost.java
@@ -0,0 +1,29 @@
+package com.swith.backend.domain.recruitmentPost.domain;
+
+import com.swith.backend.domain.recruitmentPost.types.RecruitmentPostCategory;
+import com.swith.backend.global.baseCode.BaseIdEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity
+public class RecruitmentPost extends BaseIdEntity {
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    @Column(nullable = false, columnDefinition = "tinyint")
+    private Integer RecruitmentMember;
+
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Column(nullable = false)
+    private Date endDate;
+
+    private Enum<RecruitmentPostCategory> category;
+}
---
+++ src/main/java/com/swith/backend/domain/recruitmentPost/types/RecruitmentPostCategory.java
@@ -0,0 +1,4 @@
+package com.swith.backend.domain.recruitmentPost.types;
+
+public enum RecruitmentPostCategory {
+}
