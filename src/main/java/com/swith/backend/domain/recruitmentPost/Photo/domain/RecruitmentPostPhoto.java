87edac8ecef6da939bd281e431fd06e17be020b8
luck1306 <luck1306@naver.com>
2022-07-19 20:08:46 +0900

    create (recruitment-post-photo) Entity 구현

---
+++ src/main/java/com/swith/backend/domain/recruitmentPost/Photo/domain/RecruitmentPostPhoto.java
@@ -0,0 +1,11 @@
+package com.swith.backend.domain.recruitmentPost.Photo.domain;
+
+import com.swith.backend.global.baseCode.BaseIdEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class RecruitmentPostPhoto extends BaseIdEntity {
+
+    private String path;
+}
---
+++ src/main/java/com/swith/backend/domain/recruitmentPost/Photo/repository/RecruitmentPostPhotoRepository.java
@@ -0,0 +1,7 @@
+package com.swith.backend.domain.recruitmentPost.Photo.repository;
+
+import com.swith.backend.domain.recruitmentPost.Photo.domain.RecruitmentPostPhoto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecruitmentPostPhotoRepository extends CrudRepository<RecruitmentPostPhoto, Long> {
+}
