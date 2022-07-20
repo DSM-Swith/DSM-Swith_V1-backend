1ff8f39f1cd7f9bc42c1a5788f6e3a987f93901d
luck1306 <luck1306@naver.com>
2022-07-20 21:16:02 +0900

    create (like-post) Entity 구현

---
+++ src/main/java/com/swith/backend/domain/Post/LikePost/domain/LikePost.java
@@ -0,0 +1,6 @@
+package com.swith.backend.domain.Post.LikePost.domain;
+
+import com.swith.backend.global.baseCode.BaseIdEntity;
+
+public class LikePost extends BaseIdEntity {
+}
---
+++ src/main/java/com/swith/backend/domain/Post/LikePost/repository/LikePostRepository.java
@@ -0,0 +1,7 @@
+package com.swith.backend.domain.Post.LikePost.repository;
+
+import com.swith.backend.domain.Post.LikePost.domain.LikePost;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LikePostRepository extends CrudRepository<LikePost, Long> {
+}
