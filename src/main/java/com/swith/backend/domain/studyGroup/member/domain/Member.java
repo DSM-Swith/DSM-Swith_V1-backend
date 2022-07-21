6b4a81bc36c188364753e93fac21a96fefee2d8a
hee <cmlgml094356@gmail.com>
2022-07-22 01:48:04 +0900

    delete (member) 사용하지 않는 import문 삭제

--- src/main/java/com/swith/backend/domain/studyGroup/member/domain/Member.java
+++ src/main/java/com/swith/backend/domain/studyGroup/member/domain/Member.java
@@ -11,7 +11,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
