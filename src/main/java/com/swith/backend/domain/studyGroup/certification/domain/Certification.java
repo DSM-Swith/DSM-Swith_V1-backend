e82f93e9dbad2105a1aff11e12e2727c0d4ed0c0
hee <cmlgml094356@gmail.com>
2022-07-22 01:42:39 +0900

    delete (certification) 사용하지 않는 import문 삭제

--- src/main/java/com/swith/backend/domain/studyGroup/certification/domain/Certification.java
+++ src/main/java/com/swith/backend/domain/studyGroup/certification/domain/Certification.java
@@ -2,13 +2,11 @@ package com.swith.backend.domain.studyGroup.certification.domain;
 
 import com.swith.backend.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import java.util.Date;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
