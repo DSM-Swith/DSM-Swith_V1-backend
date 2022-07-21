9f381729c4039b41f2374a7cafef555eb33ea30f
hee <cmlgml094356@gmail.com>
2022-07-22 00:05:17 +0900

    refactor (recruitmentPostPhoto) package 이동

--- src/main/java/com/swith/backend/domain/recruitmentPost/domain/repository/RecruitmentPostPhotoRepository.java
+++ src/main/java/com/swith/backend/domain/recruitmentPost/domain/repository/RecruitmentPostPhotoRepository.java
@@ -1,6 +1,6 @@
-package com.swith.backend.domain.recruitmentPost.photo.domain.repository;
+package com.swith.backend.domain.recruitmentPost.domain.repository;
 
-import com.swith.backend.domain.recruitmentPost.photo.domain.RecruitmentPostPhoto;
+import com.swith.backend.domain.recruitmentPost.domain.RecruitmentPostPhoto;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecruitmentPostPhotoRepository extends CrudRepository<RecruitmentPostPhoto, Long> {
