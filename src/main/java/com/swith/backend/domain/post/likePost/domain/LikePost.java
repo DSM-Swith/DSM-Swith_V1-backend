2efcdc8b59e45a6c501b38dfcd5c893b89c680e3
hee <cmlgml094356@gmail.com>
2022-07-21 23:44:50 +0900

    update (likePost) 어노테이션 추가

--- src/main/java/com/swith/backend/domain/post/likePost/domain/LikePost.java
+++ src/main/java/com/swith/backend/domain/post/likePost/domain/LikePost.java
@@ -2,5 +2,8 @@ package com.swith.backend.domain.post.likePost.domain;
 
 import com.swith.backend.global.entity.BaseIdEntity;
 
+import javax.persistence.Entity;
+
+@Entity
 public class LikePost extends BaseIdEntity {
 }
