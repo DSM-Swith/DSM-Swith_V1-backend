2d7df42a2ec2ba0b0bb8dfda667c6588e42af85a
hee <cmlgml094356@gmail.com>
2022-07-22 01:32:13 +0900

    update (user) 공백 생성

--- src/main/java/com/swith/backend/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/swith/backend/domain/user/domain/repository/UserRepository.java
@@ -3,4 +3,6 @@ package com.swith.backend.domain.user.domain.repository;
 import com.swith.backend.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> { }
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+}
