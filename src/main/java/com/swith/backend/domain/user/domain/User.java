9f43860734c2e655289bead76c5df07658cf4fe4
hee <cmlgml094356@gmail.com>
2022-07-25 20:17:33 +0900

    delete (user) 사용하지 않는 import문 삭제

--- src/main/java/com/swith/backend/domain/user/domain/User.java
+++ src/main/java/com/swith/backend/domain/user/domain/User.java
@@ -16,7 +16,6 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
