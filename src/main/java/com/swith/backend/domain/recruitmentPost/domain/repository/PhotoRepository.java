789e290de6b1544dbee14e0df73a00e2181904c5
hee <cmlgml094356@gmail.com>
2022-07-25 16:31:11 +0900

    update (photo) Repository 이름 수정

--- src/main/java/com/swith/backend/domain/recruitmentPost/domain/repository/PhotoRepository.java
+++ src/main/java/com/swith/backend/domain/recruitmentPost/domain/repository/PhotoRepository.java
@@ -3,5 +3,5 @@ package com.swith.backend.domain.recruitmentPost.domain.repository;
 import com.swith.backend.domain.recruitmentPost.domain.Photo;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RecruitmentPostPhotoRepository extends CrudRepository<Photo, Long> {
+public interface PhotoRepository extends CrudRepository<Photo, Long> {
 }
