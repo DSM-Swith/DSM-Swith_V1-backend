d2aa9a3a63c20419d71466d9a32ad83d22fd21d7
hee <cmlgml094356@gmail.com>
2022-07-29 20:45:32 +0900

    create (CertificationCount) Entity 구현

---
+++ src/main/java/com/swith/backend/domain/studyGroup/calculate/domin/CertificationCount.java
@@ -0,0 +1,39 @@
+package com.swith.backend.domain.studyGroup.calculate.domin;
+
+import com.swith.backend.domain.studyGroup.member.domain.Member;
+import com.swith.backend.global.entity.BaseIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class CertificationCount extends BaseIdEntity {
+
+    @Column(nullable = false, columnDefinition = "tinyint")
+    private Integer certificationCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "calculate_id")
+    private Calculate calculate;
+
+    @Builder
+    public CertificationCount(Integer certificationCount, Member member, Calculate calculate) {
+        this.certificationCount = certificationCount;
+        this.member = member;
+        this.calculate = calculate;
+    }
+
+}
---
+++ src/main/java/com/swith/backend/domain/studyGroup/calculate/domin/repository/CertificationCountRepository.java
@@ -0,0 +1,7 @@
+package com.swith.backend.domain.studyGroup.calculate.domin.repository;
+
+import com.swith.backend.domain.studyGroup.calculate.domin.CertificationCount;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CertificationCountRepository extends CrudRepository<CertificationCount, Long> {
+}
