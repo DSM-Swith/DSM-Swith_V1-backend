d700a419b8d458672b1d6848ad61e317ba2e4f12
hee <cmlgml094356@gmail.com>
2022-08-01 15:24:03 +0900

    update (certificationCount) Repository 생성
    CertificationRepository를 삭제해야 했는데 CertificationCountRepository를 삭제함...

--- src/main/java/com/swith/backend/domain/studyGroup/calculate/domin/repository/CalculateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.swith.backend.domain.studyGroup.calculate.domin.repository;
-
-import com.swith.backend.domain.studyGroup.calculate.domin.Calculate;
-import com.swith.backend.domain.studyGroup.studyGroup.domain.StudyGroup;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CalculateRepository extends CrudRepository<Calculate, StudyGroup> {
-}
